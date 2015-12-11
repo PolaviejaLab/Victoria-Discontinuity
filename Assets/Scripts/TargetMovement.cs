29ca381ce857d29bbb9732816847a9938e275b83
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2015-12-11 15:40:54 +0000

    Knife added

---
+++ Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetMovement : MonoBehaviour {
+
+
+	public GameObject target; 
+	private float speed;
+	private Vector3 movement;
+
+
+	void Start () {
+		NewMovements ();
+	}
+	
+	void Update () {
+		target.transform.Translate (movement*speed);
+		if (target.transform.position.x > 4.75 || 
+		    target.transform.position.z > 4.75 || 
+		    target.transform.position.x < -4.75 || 
+		    target.transform.position.z < -4.75) {
+			NewMovements ();
+			}
+	}
+	void NewMovements () {
+		movement = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+		speed = Random.Range (0.0005f, 0.0010f);
+	}
+}
---
+++ Assets/Scripts/Threat.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Threat : MonoBehaviour 
+{
+	private enum ThreatState { Idle, Falling, Following };
+
+
+
+	// public Transform targetMovement;
+	public MarkerController markerController;
+
+	public GameObject threat;
+
+	private Vector3 targetPosition;
+	private Vector3 threatPosition;
+	
+	private float targetPositionx;
+	private float targetPositiony;
+	private float targetPositionz;
+
+	public Transform handTransform;
+
+	public Vector3 handPosition;
+
+	private float threatSpeed;
+	private float gravity = 9.81f;
+
+
+	private ThreatState threatState;
+	Quaternion initialThreatRotation;
+	Quaternion savedRotation;
+
+	// Use this for initialization
+	void Start () {
+		initialThreatRotation = threat.transform.rotation;
+		threatState = ThreatState.Idle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// targetPositionx = targetMovement.position.x;
+		// targetPositiony = targetMovement.position.y;
+		// targetPositionz = targetMovement.position.z;
+		// Debug.Log (targetMovement.position);
+		handPosition = handTransform.position;
+		Debug.Log (handTransform.position);
+
+		if (Input.GetKeyDown ("space") && threatState == ThreatState.Idle) {
+			Debug.Log ("Knife falling");
+			threatState = ThreatState.Falling;
+			threatSpeed = 0;
+		}
+
+		if (threatState == ThreatState.Falling) {
+
+			if(Vector3.Distance(threat.transform.position, handTransform.position) < 0.001) {
+				threatState = ThreatState.Following;
+				savedRotation = handTransform.rotation;
+			}
+
+			FallOnTarget ();
+		}
+
+		if (threatState == ThreatState.Following) {
+			threat.transform.position = handTransform.position;
+
+			threat.transform.rotation = (handTransform.rotation * Quaternion.Inverse(savedRotation)) * initialThreatRotation;
+		}
+	}
+
+	void FallOnTarget () {
+		threatSpeed += gravity * Time.deltaTime;
+
+			threat.transform.position = Vector3.MoveTowards (
+				threat.transform.position, 
+				handTransform.position, 
+				threatSpeed * Time.deltaTime);
+	}
+}
