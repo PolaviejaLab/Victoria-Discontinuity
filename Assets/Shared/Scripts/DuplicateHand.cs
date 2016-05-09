e48260992d5fa5d18efd8bc3708e516d8d9d2f35
Ivar Clemens <post@ivarclemens.nl>
2016-05-09 16:18:07 +0100

    Script to duplicate hand position and orientation.

---
+++ Assets/Shared/Scripts/DuplicateHand.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Leap.Unity;
+using System.Collections;
+
+/**
+ * Structure of the object:
+ *
+ *  - BaseObject
+ *   - Skinned mesh renderer
+ *   - HandContainer
+ *    - Hand
+ *     - Finger (x5)
+ *    - ForearmContainer (optional)
+ */
+public class DuplicateHand : MonoBehaviour
+{
+    public IHandModel template;
+
+    public int min(int a, int b)
+    {
+        if (a > b)
+            return b;
+        return a;
+    }
+
+    public void CopyTransform(Transform src, Transform dst)
+    {
+        for (int i = 0; i < min(src.childCount, dst.childCount); i++)
+        {            
+            Transform srcT = src.GetChild(i);
+            Transform dstT = dst.GetChild(i);
+
+            CopyTransform(srcT, dstT);
+        }
+
+        dst.localPosition = src.localPosition;
+        dst.localRotation = src.localRotation;
+    }
+
+    public void Update()
+    {
+        CopyTransform(template.transform, transform);
+    }
+}
