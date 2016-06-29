012a06d0fc0d1f935801d99604202e5a0518e770
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2016-06-29 19:01:50 +0100

    Inactive trial state machine

---
+++ Assets/InactiveTrialStateMachine.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InactiveTrialStateMachine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
