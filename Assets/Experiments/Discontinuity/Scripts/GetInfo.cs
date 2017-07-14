385f53a780839aff63029a48897fe3f5b43dae49
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2017-07-14 20:51:05 +0100

    added canvias

---
+++ Assets/Experiments/Discontinuity/Scripts/GetInfo.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetInfo : MonoBehaviour
+{
+
+    public HandSwitcher handSwitcher;
+    public ExperimentController experimentController;
+
+    public string subjectCode;
+    public int expNum;
+    public string experimentName;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void getCode(string subjectName)
+    {
+        subjectCode = subjectName;
+        Debug.Log("Subject Code " + subjectCode);
+    }
+
+    public void isMale(bool male)
+    {
+        handSwitcher.useMale = true;
+        Debug.Log("Changed gender to male");
+    }
+
+    public void isFemale(bool female)
+    {
+        handSwitcher.useMale = false;
+        Debug.Log("Changed gender to female");
+    }
+    
+    public void startExperiment()
+    {
+        experimentController.ChangeState(ExperimentStates.Start);
+        Debug.Log("Experiment has been started");
+    }
+}
---
+++ Assets/Experiments/Discontinuity/Scripts/getExperimentNumber.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getExperimentNumber : MonoBehaviour {
+
+    public string experimentName;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void getNumber(int expNum)
+    {
+        Debug.Log("int passed: " + expNum);
+        if (expNum == 1)
+        {
+            experimentName = "Exp2_Repetition";
+        }
+
+        Debug.Log("This program will load protocol for " + experimentName);
+    }
+}
